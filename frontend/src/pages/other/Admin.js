c930df22bc246e085baad8e13e178bb357c8e43a
qw-jl <joseluis@quitiweb.com>
2020-04-16 10:59:29 +0200

    Subida correcta de productos con su imagen desde el frontend

--- frontend/src/pages/other/Admin.js
+++ frontend/src/pages/other/Admin.js
@@ -1,45 +1,66 @@
-import React, {Fragment, useState} from 'react'
+import React, {Fragment, useEffect, useState} from 'react'
 import LayoutOne from "../../layouts/LayoutOne";
 import MetaTags from "react-meta-tags";
-import { connect } from "react-redux";
-import { getProducts } from "../../helpers/product";
-import { addToCart } from "../../redux/actions/cartActions";
-import { addToWishlist } from "../../redux/actions/wishlistActions";
-import { addToCompare } from "../../redux/actions/compareActions";
 import axios from 'axios';
 
 const Admin = ({
-    products,
-    currency,
-    addToCart,
-    addToWishlist,
-    addToCompare,
-    cartItems,
-    wishlistItems,
-    compareItems,
-    sliderClassName,
-    spaceBottomClass
 }) => {
 
+    var [products, setProducts] = useState([]);
+
+    
+    useEffect(() => {
+        recargarVista();
+    },[]) 
+
+    var recargarVista = () => {
+        axios.get('http://127.0.0.1:8000/api')
+        .then(function (response) {
+          setProducts(response.data)
+          console.log(response.data);
+          products.map((producto)=>{  
+              producto.id = producto.id.toString()
+              producto.sku = producto.sku.toString()
+              producto.image = [producto.image]
+              producto.category = [producto.category]
+              producto.tag = [producto.tag]
+          }); 
+          console.log(products)
+          
+        })
+        .catch(function (error) {
+          console.log(error);
+        });        
+    }
+ 
     var onClickButton = () => {
 
+        console.log(document.getElementById('form-image').files[0])
 
-        axios.post('http://127.0.0.1:8000/api/', {
-            sku: '111',
-            name: 'Producto de prueba',
-            price: 1,
-            discount: 5,
-            new: false,
-            rating: 0.0,
-            saleCount: 0,
-            category: 'na',
-            tag: 'na',
-            stock: 150,
-            image: null,
-            shortDescription: "na",
-            fullDescription: "naaaaaaaaaaaaaa"
-          })
+        let form_data = new FormData();
+        form_data.append('sku', document.getElementById('form-sku').value)
+        form_data.append('name', document.getElementById('form-nombre').value)
+        form_data.append('price', document.getElementById('form-precio').value)
+        form_data.append('discount', document.getElementById('form-descuento').value)
+        form_data.append('new', true)
+        form_data.append('rating', 0.0)
+        form_data.append('saleCount', 0)
+        form_data.append('category', document.getElementById('form-categorias').value)
+        form_data.append('tag', document.getElementById('form-etiquetas').value)
+        form_data.append('stock', document.getElementById('form-stock').value)
+        form_data.append('image', document.getElementById('form-image').files[0])
+        form_data.append('shortDescription', document.getElementById('form-breve').value)
+        form_data.append('fullDescription', document.getElementById('form-full').value)
+        
+        axios.post('http://127.0.0.1:8000/api/',form_data, {
+            headers: {
+              'accept': 'application/json',
+              'Accept-Language': 'en-US,en;q=0.8',
+              'Content-Type': 'multipart/form-data;'
+            }
+        })
           .then(function (response) {
+            recargarVista();
             console.log(response);
           })
           .catch(function (error) {
@@ -97,7 +118,7 @@ const Admin = ({
                 </div>
                     <div className="col-1"></div>
 
-                    <form className="col-12 col-lg-10" method="POST">
+                    <form className="col-12 col-lg-10" method="POST" encType="multipart/form-data">
                         <div className="row">
                             <div className="col-12 mt-2">
                                 <input type="text" placeholder="Nombre" id="form-nombre"/>
@@ -109,7 +130,7 @@ const Admin = ({
                                 <input type="text" placeholder="Precio" id="form-precio"/>
                             </div>
                             <div className="col-6 mt-2">
-                                <input type="text" placeholder="% Descuento" id="form-decuento"/>
+                                <input type="text" placeholder="% Descuento" id="form-descuento"/>
                             </div>
                             
                             <div className="col-6 mt-2">
@@ -129,11 +150,11 @@ const Admin = ({
                             </div>
 
                             <div className="col-12">
-                                <input type="text" placeholder="Descripción breve del producto"id="form-"/>
+                                <input type="text" placeholder="Descripción breve del producto"id="form-breve"/>
                             </div>
 
                             <div className="col-12 mt-3">
-                                <textarea name="" id="" cols="30" rows="10" placeholder="Descripción completa del producto">
+                                <textarea name="" id="form-full" cols="30" rows="10" placeholder="Descripción completa del producto">
 
                                 </textarea>
                             </div>
@@ -149,48 +170,6 @@ const Admin = ({
     )
 }
 
-const mapStateToProps = (state, ownProps) => {
-    return {
-      products: getProducts(
-        state.productData.products,
-        ownProps.category,
-        ownProps.type,
-        ownProps.limit
-      ),
-      currency: state.currencyData,
-      cartItems: state.cartData,
-      wishlistItems: state.wishlistData,
-      compareItems: state.compareData
-    };
-  };
-  
-  const mapDispatchToProps = dispatch => {
-    return {
-      addToCart: (
-        item,
-        addToast,
-        quantityCount,
-        selectedProductColor,
-        selectedProductSize
-      ) => {
-        dispatch(
-          addToCart(
-            item,
-            addToast,
-            quantityCount,
-            selectedProductColor,
-            selectedProductSize
-          )
-        );
-      },
-      addToWishlist: (item, addToast) => {
-        dispatch(addToWishlist(item, addToast));
-      },
-      addToCompare: (item, addToast) => {
-        dispatch(addToCompare(item, addToast));
-      }
-    };
-  };
 
 
-  export default connect(mapStateToProps, mapDispatchToProps)(Admin);
+  export default Admin;
