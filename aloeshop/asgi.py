4812f73577d9f8f8b0d7726c50b216aa0a1da8fb
qw-jl <joseluis@quitiweb.com>
2020-05-08 12:44:47 +0200

    Cambio de aloe_api a aloeshop

--- aloeshop/asgi.py
+++ aloeshop/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for aloe_api project.
+ASGI config for aloeshop project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aloe_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aloeshop.settings')
 
 application = get_asgi_application()
--- aloeshop/urls.py
+++ aloeshop/urls.py
@@ -1,4 +1,4 @@
-"""aloe_api URL Configuration
+"""aloeshop URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
